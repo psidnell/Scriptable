1d27827bd365deb882554171d7e280c85bdb9dc4
Paul Sidnell <psidnell@users.noreply.github.com>
2021-08-21 08:10:25 +0100

    Tweaks to nominated projects

--- Calendars.js
+++ Calendars.js
@@ -26,12 +26,12 @@
 DAYS_TO_SHOW = 150;
 
 // The default project path for creating OmniFocus events
-DEFAULT_PROJECT = ['[Home Misc]', '[Calendar]'].join(' : ');
+DEFAULT_PROJECT = ['Home', '[Calendar]'].join(' : ');
 
 // Here you can create mappings so that different calendars create Omnifocus events in different projects.
 const PROJECT_MAP = {
-    'Calendar': ['[Work Misc]', '[Calendar]'].join(' : '),
-    'Work': ['[Work Misc]', '[Calendar]'].join(' : ')
+    'Calendar': ['Work', '[Calendar]'].join(' : '),
+    'Work': ['Work', '[Calendar]'].join(' : ')
 };
 
 // The default tag path for creating OmniFocus events, can be null
